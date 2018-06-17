123c724eb81deeac9c486c891505b7e97313cffe
juanixou <juansilvamora@gmail.com>
2018-06-17 09:52:56 +0200

    Segunda subida

---
+++ Web y Redes Sociales Junio/Assets/Scripts/CambiosEscena.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambiosEscena : MonoBehaviour {
+
+    public void CargarContacto()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CargarListaNiveles()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void CargarNivel1()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void CargarNivel2()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void CargarMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+
+}
---
+++ Web y Redes Sociales Junio/Assets/Scripts/InstagramRedirect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class InstagramRedirect : MonoBehaviour {
+
+    public void RedirectInsta()
+    {
+        Application.OpenURL("https://www.instagram.com/juandsilvamora/");
+    }
+
+    public void RedirectGitHub()
+    {
+        Application.OpenURL("https://github.com/Juanixou");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
